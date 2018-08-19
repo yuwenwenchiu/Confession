b505c51277d5f81b9025d09ea208c52774808dde
Yuwen Chiu <yuwenchiu@yuwende-macbook-pro.local>
2018-08-20 00:02:10 +0800

    增加註解

--- Confession/ViewController.swift
+++ Confession/ViewController.swift
@@ -11,18 +11,22 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    //所有outlet
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var rateSlider: UISlider!
     @IBOutlet weak var rateText: UILabel!
     @IBOutlet weak var pitchSlider: UISlider!
     @IBOutlet weak var pitchText: UILabel!
-    
-    
+    //語速和語調依照slider滑動顯示不同數字
     @IBAction func changeSlider(_ sender: Any) {
         rateText.text = String(format: "%.1f", rateSlider.value)
         pitchText.text = String(format: "%.1f", pitchSlider.value)
     }
-    
+    //按鈕動作：讀取textField的內容說話
     @IBAction func buttonPressed(_ sender: Any) {
         let speechUtterance = AVSpeechUtterance(string: textField.text!)
         let synthesizer = AVSpeechSynthesizer()
@@ -32,10 +36,5 @@ class ViewController: UIViewController {
         synthesizer.speak(speechUtterance)
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
 }
 
